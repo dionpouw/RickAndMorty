f192b293f38bbf079e86c03d6b2691c37d35a3a1
Aldion <aldion.sumampouw@gmail.com>
2024-09-17 15:09:18 +0800

    update: Rename class since it's the same class as the other one.

--- app/src/main/java/com/jefflete/rickandmorty/data/network/response/episode/EpisodesResponse.kt
+++ app/src/main/java/com/jefflete/rickandmorty/data/network/response/episode/EpisodesResponse.kt
@@ -7,50 +7,19 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class EpisodesResponse(
 
-    @field:SerializedName("results")
-    val results: List<ResultsItem>,
+    @field:SerializedName("results") val results: List<EpisodeResponse>,
 
-    @field:SerializedName("info")
-    val info: Info
-) : Parcelable
-
-@Parcelize
-data class ResultsItem(
-
-    @field:SerializedName("air_date")
-    val airDate: String,
-
-    @field:SerializedName("characters")
-    val characters: List<String>,
-
-    @field:SerializedName("created")
-    val created: String,
-
-    @field:SerializedName("name")
-    val name: String,
-
-    @field:SerializedName("episode")
-    val episode: String,
-
-    @field:SerializedName("id")
-    val id: Int,
-
-    @field:SerializedName("url")
-    val url: String
+    @field:SerializedName("info") val info: Info
 ) : Parcelable
 
 @Parcelize
 data class Info(
 
-    @field:SerializedName("next")
-    val next: String,
+    @field:SerializedName("next") val next: String,
 
-    @field:SerializedName("pages")
-    val pages: Int,
+    @field:SerializedName("pages") val pages: Int,
 
-    @field:SerializedName("prev")
-    val prev: String?,
+    @field:SerializedName("prev") val prev: String?,
 
-    @field:SerializedName("count")
-    val count: Int
+    @field:SerializedName("count") val count: Int
 ) : Parcelable
--- app/src/main/java/com/jefflete/rickandmorty/data/network/response/location/LocationsResponse.kt
+++ app/src/main/java/com/jefflete/rickandmorty/data/network/response/location/LocationsResponse.kt
@@ -7,50 +7,19 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class LocationsResponse(
 
-    @field:SerializedName("results")
-    val results: List<ResultsItem>,
+    @field:SerializedName("results") val results: List<LocationResponse>,
 
-    @field:SerializedName("info")
-    val info: Info
+    @field:SerializedName("info") val info: Info
 ) : Parcelable
 
 @Parcelize
 data class Info(
 
-    @field:SerializedName("next")
-    val next: String,
+    @field:SerializedName("next") val next: String,
 
-    @field:SerializedName("pages")
-    val pages: Int,
+    @field:SerializedName("pages") val pages: Int,
 
-    @field:SerializedName("prev")
-    val prev: String?,
+    @field:SerializedName("prev") val prev: String?,
 
-    @field:SerializedName("count")
-    val count: Int
-) : Parcelable
-
-@Parcelize
-data class ResultsItem(
-
-    @field:SerializedName("created")
-    val created: String,
-
-    @field:SerializedName("name")
-    val name: String,
-
-    @field:SerializedName("residents")
-    val residents: List<String>,
-
-    @field:SerializedName("id")
-    val id: Int,
-
-    @field:SerializedName("type")
-    val type: String,
-
-    @field:SerializedName("dimension")
-    val dimension: String,
-
-    @field:SerializedName("url")
-    val url: String
+    @field:SerializedName("count") val count: Int
 ) : Parcelable
